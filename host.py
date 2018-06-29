e43fbbed68f067d491c957502eb7ba49f1b81d66
Umarahmad87 <umarahmadsiddiqi@outlook.com>
2018-06-29 11:26:07 +0000

    Add files via upload

--- host.py
+++ host.py
@@ -7,7 +7,7 @@ import numpy as np
 # Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
 # all interfaces)
 server_socket = socket.socket()
-server_socket.bind(('192.168.200.100', 777))
+server_socket.bind(('192.168.200.102', 2036))
 server_socket.listen(0)
 
 # Accept a single connection and make a file-like object out of it
@@ -19,6 +19,9 @@ try:
         image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
         if not image_len:
             break
+        xc = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        yc = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        print 'x:',xc,'y:',yc
         # Construct a stream to hold the image data and read the image
         # data from the connection
         image_stream = io.BytesIO()
@@ -27,10 +30,12 @@ try:
         # processing on it
         image_stream.seek(0)
         #image = Image.open(image_stream)
+        
 
         data = np.fromstring(image_stream.getvalue(), dtype=np.uint8)
         imagedisp = cv2.imdecode(data, 1)
-
+        imagedisp = cv2.circle(imagedisp,(int(320),int(240)),2,(255,0,0),2)
+        imagedisp = cv2.circle(imagedisp,(int(yc),int(xc)),5,(0,0,255),3)   
         cv2.imshow("Frame",imagedisp)
         #cv2.waitKey(1)  #imshow will not output an image if you do not use waitKey
         #cv2.destroyAllWindows() #cleanup windows
