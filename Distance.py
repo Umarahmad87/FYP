d1cdca8104eaa22a2492fc10d848dc20460d274e
Umarahmad87 <umarahmadsiddiqi@outlook.com>
2018-07-11 13:45:54 +0500

    Add files via upload

--- Distance.py
+++ Distance.py
@@ -12,7 +12,7 @@ class CDistance:
         #frame = cv2.circle(frame, (320, 240), 1, (255,0,0), 3)
         #print 'frame.shape=',frame.shape
         try:
-            num = (frame[:,:,2] <= 255) & (frame[:,:,2] >= 210) & (frame[:,:,0] <= 180) & (frame[:,:,0] >= 0) & (frame[:,:,1] <= 180) & (frame[:,:,1] >= 0)
+            num = (frame[:,:,2] <= 255) & (frame[:,:,2] >= 200) & (frame[:,:,0] <= 180) & (frame[:,:,0] >= 0) & (frame[:,:,1] <= 180) & (frame[:,:,1] >= 0)
             #lower = array([50,50,170])
             #upper = array([255,255,180])
             #num = cv2.inRange(frame,lower,upper)
@@ -21,30 +21,32 @@ class CDistance:
             #print 'num.shape:',num.shape
             xy_val =  num.nonzero()
             #print "xyLen=",len(xy_val)
-            x_val = median(xy_val[0])
-            y_val = median(xy_val[1])
+            x_val = median(xy_val[1])
+            y_val = median(xy_val[0])
             #print 'x:',x_val
             #print 'y:',y_val
             if not(math.isnan(x_val) or math.isnan(y_val)): 
                     #print 'RGB:',frame[int(x_val),int(y_val)]
                     #frame = cv2.circle(frame, (int(y_val), int(x_val)), 5, (0,0,255), 3)
                     
-                    dist = ((x_val - 320)**2 + (y_val - 240)**2 )**0.5 # distance of dot from center pixel
-                    #dist = abs(x_val - 320) # distance of dot from center x_axis only
+                    dist = ((x_val - 160)**2 + (y_val - 120)**2 )**0.5 # distance of dot from center pixel
+                    #dist = abs(x_val - 160) # distance of dot from center x_axis only
 
                     print " dist from center pixel is " + str(dist)
 
                     # work out distance using D = h/tan(theta)
 
-                    theta =0.00148933*dist + 0.11908426
-                    print 'theta=',theta
+                    #theta =0.00148933*dist + 0.11908426
+                    theta =0.002669874773130*dist + 0.05937000706031
+                    #print 'theta=',theta
                     tan_theta = math.tan(theta)
                     
                     if tan_theta > 0: # bit of error checking
-                            self.obj_dist =  int(21.0 / tan_theta)
+                            self.obj_dist =  int(3.50 / tan_theta)
 
                             print "\033[12;0H" + "the dot is " + str(self.obj_dist) + "cm  away"
                             return frame,x_val,y_val
+            print "not detected"
             return frame,0,0
         except:
             print "frame empty"
