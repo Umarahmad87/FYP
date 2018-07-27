b453dccf0e66075e2d4e1443a6900f3523f12f93
Umarahmad87 <umarahmadsiddiqi@outlook.com>
2018-07-27 09:32:29 +0000

    Add files via upload

--- client.py
+++ client.py
@@ -16,7 +16,7 @@ from Queue import Queue
 from robot import *
 from Robot_Canvas import *
 
-ip = "192.168.200.125"
+ip = "192.168.10.125"
 port = 3051
 camera = 0
 connection=0
@@ -41,50 +41,126 @@ def signal_handler(signal,frame):
 
 signal.signal(signal.SIGINT,signal_handler)
 distance = CDistance()
+
+def get_distance():
+    U_dist = R.sonic_distance()
+    return U_dist
+    print "sonic distance:",U_dist
+    try:
+        dist = t_stream.get()
+    except:
+        dist = U_dist
+    print "laser distance:",dist
+    
+    if dist<=35 and U_dist<=50:
+        dist = (dist+U_dist)/2
+    elif dist>=35 and U_dist<=50:    
+        dist = U_dist
+    elif dist<=35 and U_dist>50:    
+        dist = dist
+    else:
+        dist = 100
+    return dist
+
+def get_360_readings():
+    #positions = ['right','backward','left','forward']
+    positions = canvas.get_next_directions()
+    print '360'
+    dista360 = []
+    for pos in positions:
+        move_right(90)
+        #R.right(0.6,speed=30)
+        canvas.update_direction(pos)
+
+        dist = get_distance()
+        dista360.append(dist)
+        print "dist:",dist
+        
+        time.sleep(0.5)
+        
+        canvas.set_obstacle(dist)
+    return dista360,positions
+
+def move_right(deg):
+    if deg==90:
+        R.right(0.7,speed=30)
+    elif deg==180:
+        R.right(1.2,speed=30)
+
+def rotate_to_direction(direction1):
+    c_dir = canvas.get_direction()
+    print 'direction to move:',direction1
+    print 'current direction:',c_dir
+    canvas.update_direction(direction1)
+    directions = ['forward','backward','right','left']
+    Move_Matrix = [  [(0,0,0),(90,90,0),(90,0,0),(90,90,90)], [(90,90,0),(0,0,0),(90,90,90),(90,0,0)], [(90,90,90),(90,0,0),(0,0,0),(90,90,0)], [(90,0,0),(90,90,90),(90,90,0),(0,0,0)]]
+    rr=directions.index(c_dir)
+    cc=directions.index(direction1)
+    moves=Move_Matrix[rr][cc]
+    print 'moves:',moves
+    move_right(moves[0])
+    move_right(moves[1])
+    move_right(moves[2])
+
 def image_show():
     count_range=0
     in_range = False
     dist = 0
+    positions = ['right','backward','left','forward']
+    bool1 = True
     while True:
         try:
-            U_dist = R.sonic_distance()
-            print "sonic distance:",U_dist
-            try:
-                dist = t_stream.get()
-            except:
-                dist = U_dist
-            print "laser distance:",dist
+             
+            if bool1==True:
+                dists,positiona = get_360_readings()
+                dir_to_move = positiona[np.argmax(dists)]
+                rotate_to_direction(dir_to_move)
+                print 'moving to:',dir_to_move
+                #canvas.update_position()
+                canvas.write_to_file()    
+                bool1=False
+                print 'sleep'
+                time.sleep(1)
+                print 'awake from sleep'
             
-            if dist<=35 and U_dist<=50:
-                dist = (dist+U_dist)/2
-            elif dist>=35 and U_dist<=50:    
-                dist = U_dist
-            else:
-                dist = 100
-            print "dist:",dist
-
-            if dist>=35:
-                if in_range == False:
-                    R.forward(0.1)
-                    canvas.update_direction('forward')
-                    canvas.update_position()
+            print 'forward'
+            R.forward(0.3,speed=30)
+            try:
+                canvas.update_position()
+            except:
+                print 'exception in canvas update position'
+        
+            dist = get_distance()
+            print 'dist:',dist
+        
+            if dist<=35:
+                bool1=True
+                #print 'break break break'
+                #break
+            
+            #if dist>=35:
+            #    if in_range == False:
+            #        R.forward(0.1)
+            #        canvas.update_direction('forward')
+            #        canvas.update_position()
                 #print "out of range"
-                if count_range>=10:
-                    in_range = False
-                    count_range = 0 
+            #    if count_range>=10:
+            #        in_range = False
+            #        count_range = 0 
                 
-                if in_range == True:
-                    count_range+=1
-            else:
-                #R.right(1)
-                in_range = True
-                count_range = 0
+            #    if in_range == True:
+            #        count_range+=1
+            #else:
+            #    R.right(2)
+            #    in_range = True
+            #    count_range = 0
                 #print "in range"
-                canvas.write_to_file()
-            canvas.set_obstacle(dist)
+            #    canvas.write_to_file()
+            
     
             try:
                 bool_thread = tstop.get()
+                
             except:
                 print "hello"
                 pass
@@ -96,7 +172,8 @@ def image_show():
             canvas.write_to_file()
             #break
             sys.exit(0)
-
+        finally:
+            canvas.write_to_file()
 
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
