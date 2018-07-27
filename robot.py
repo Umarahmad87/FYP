60a93566966ce21ffd969f07b3bf712a66132e09
Umarahmad87 <umarahmadsiddiqi@outlook.com>
2018-07-27 09:34:08 +0000

    Add files via upload

--- robot.py
+++ robot.py
@@ -7,66 +7,132 @@ class RoboCar:
             gpio.cleanup()
         finally:
             self.mode = gpio.BOARD
-            self.yellow = pin[0]
-            self.brown = pin[1]
-            self.red  = pin[2]
-            self.orange = pin[3]
+            #self.yellow = pin[0]
+            #self.brown = pin[1]
+            #self.red  = pin[2]
+            #self.orange = pin[3]
+            
+            self.L1 = pin[0] # yellow
+            self.L2 = pin[3] # orange
+            self.R1 = pin[2] # red
+            self.R2 = pin[1] # brown
+            
+            self.p=0
+            self.q=0
+            self.a=0
+            self.b=0
+        
             self.GPIO_TRIGGER = pin[4]
             self.GPIO_ECHO = pin[5]
             self.setup()
+            
+            
         
     def setup(self):
-        gpio.setmode(gpio.BOARD)
+        """gpio.setmode(gpio.BOARD)
         gpio.setup(self.yellow,gpio.OUT)
         gpio.setup(self.brown,gpio.OUT)
         gpio.setup(self.red,gpio.OUT)
-        gpio.setup(self.orange,gpio.OUT)
+        gpio.setup(self.orange,gpio.OUT)"""
+        
+        gpio.setmode(self.mode)
         gpio.setup(self.GPIO_TRIGGER, gpio.OUT)
         gpio.setup(self.GPIO_ECHO, gpio.IN)
         
-    def backward(self,step=0):
-        gpio.output(self.yellow,False)
-        gpio.output(self.brown,True)
-        gpio.output(self.red,False)
-        gpio.output(self.orange,True)
+        
+        #use pwm on inputs so motors don't go too fast
+        gpio.setup(self.L1, gpio.OUT)
+        self.p = gpio.PWM(self.L1, 20)
+        self.p.start(0)
+
+        gpio.setup(self.L2, gpio.OUT)
+        self.q = gpio.PWM(self.L2, 20)
+        self.q.start(0)
+
+        gpio.setup(self.R1, gpio.OUT)
+        self.a = gpio.PWM(self.R1, 20)
+        self.a.start(0)
+
+        gpio.setup(self.R2, gpio.OUT)
+        self.b = gpio.PWM(self.R2, 20)
+        self.b.start(0)
+
+        
+    def backward(self,step=0,speed=10):
+        #gpio.output(self.yellow,False)
+        #gpio.output(self.brown,True)
+        #gpio.output(self.red,False)
+        #gpio.output(self.orange,True)
+        
+        self.p.ChangeDutyCycle(0)
+        self.q.ChangeDutyCycle(speed)
+        self.a.ChangeDutyCycle(0)
+        self.b.ChangeDutyCycle(speed)
+        #q.ChangeFrequency(speed + 5)
+        #b.ChangeFrequency(speed + 5)
         if step>0:
             time.sleep(step)
             self.Break()
         #self.left(0.1)
         return
-    def forward(self,step=0.0):
-        gpio.output(self.yellow,True)
-        gpio.output(self.brown,False)
-        gpio.output(self.red,True)
-        gpio.output(self.orange,False)
+    def forward(self,step=0.0,speed=10):
+        #gpio.output(self.yellow,True)
+        #gpio.output(self.brown,False)
+        #gpio.output(self.red,True)
+        self.p.ChangeDutyCycle(speed)
+        self.q.ChangeDutyCycle(0)
+        self.a.ChangeDutyCycle(speed)
+        self.b.ChangeDutyCycle(0)
+        #p.ChangeFrequency(speed + 5)
+        #a.ChangeFrequency(speed + 5)#gpio.output(self.orange,False)
+            
         if step>0:
             time.sleep(step)
             self.Break()
         #self.right(0.13)
         return
-    def left(self,step=0):
-        gpio.output(self.yellow,True)
-        gpio.output(self.brown,True)
-        gpio.output(self.red,False)
-        gpio.output(self.orange,False)
+    def left(self,step=0,speed=10):
+        #gpio.output(self.yellow,True)
+        #gpio.output(self.brown,True)
+        #gpio.output(self.red,False)
+        #gpio.output(self.orange,False)
+        
+        self.p.ChangeDutyCycle(speed)
+        self.q.ChangeDutyCycle(0)
+        self.a.ChangeDutyCycle(0)
+        self.b.ChangeDutyCycle(speed)
+        #p.ChangeFrequency(speed + 5)
+        #b.ChangeFrequency(speed + 5)
         if step>0:
             time.sleep(step)
             self.Break()
         return
-    def right(self,step=0):
-        gpio.output(self.yellow,False)
-        gpio.output(self.brown,False)
-        gpio.output(self.red,True)
-        gpio.output(self.orange,True)
+    def right(self,step=0,speed=10):
+        #gpio.output(self.yellow,False)
+        #gpio.output(self.brown,False)
+        #gpio.output(self.red,True)
+        #gpio.output(self.orange,True)
+        
+        self.p.ChangeDutyCycle(0)
+        self.q.ChangeDutyCycle(speed)
+        self.a.ChangeDutyCycle(speed)
+        self.b.ChangeDutyCycle(0)
+        #q.ChangeFrequency(speed + 5)
+        #a.ChangeFrequency(speed + 5)
+        
         if step>0:
             time.sleep(step)
             self.Break()
         return
     def Break(self):
-        gpio.output(self.yellow,False)
-        gpio.output(self.brown,False)
-        gpio.output(self.red,False)
-        gpio.output(self.orange,False)
+        self.p.ChangeDutyCycle(0)
+        self.q.ChangeDutyCycle(0)
+        self.a.ChangeDutyCycle(0)
+        self.b.ChangeDutyCycle(0)
+        #gpio.output(self.yellow,False)
+        #gpio.output(self.brown,False)
+        #gpio.output(self.red,False)
+        #gpio.output(self.orange,False)
         return
     def reset(self):
         gpio.cleanup()
