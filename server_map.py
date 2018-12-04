22ac1bf721a4b2c9d0e16c3c3447dee464237121
Asmar <40683799+AsmarMuqeet@users.noreply.github.com>
2018-12-04 15:50:37 +0500

    Add files via upload

--- server_map.py
+++ server_map.py
@@ -6,23 +6,48 @@ import numpy as np
 from scipy.misc import imsave
 import ast
 import pandas as pd
-
+import cv2
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('192.168.100.81', 1236))
+s.bind(('192.168.0.193', 1215))
 s.listen(1)
 conn, addr = s.accept()
 b = b''
 print 'connected'
 k = 1
+kernel = np.ones((2,6),np.float32)#/31
+kernel2 = np.ones((3,3), np.uint8)
 while 1:
     tmp = conn.recv(1024)
     s = ast.literal_eval(tmp)
     print s
-    K = np.zeros(tuple(s["size"]),dtype='int16')
+    if(len(s["2"])==0):
+        s["2"] = s["1"]
+    if(len(s["3"])==0):
+        s["3"] = s["1"] 	
     data = pd.DataFrame(s["2"],columns=['row','col'],dtype='int16')
-    K[[data['row'].values],[data['col'].values]] =255
-    imsave('map'+str(k)+'.png',K)
+    data2 = pd.DataFrame(s["1"],columns=['row','col'],dtype='int16')
+    data3 = pd.DataFrame(s["3"],columns=['row','col'],dtype='int16')
+    maxrow = int(max([data['row'].max(),data2['row'].max(),data3['row'].max()]))
+    maxcol = int(max([data['col'].max(),data2['col'].max(),data3['col'].max()]))
+    minrow = int(min([data['row'].min(),data2['row'].min(),data3['row'].min()]))
+    mincol = int(min([data['col'].min(),data2['col'].min(),data3['col'].min()]))
+    print "maxrow :",maxrow," minrow :",minrow
+    print "maxcol :",maxcol," mincol :",mincol
+    K = np.zeros((maxrow+20,maxcol+20,3),dtype='int16') # maxrow+20
+    K[[data['row'].values],[data['col'].values]] =[255,0,0]
+    K[[data3['row'].values],[data3['col'].values]] =[128,255,128]
+    K[[data2['row'].values],[data2['col'].values]] =[0,0,255]
+    RA = K[minrow:maxrow+1,mincol:maxcol+1,:]	
+    
+    #print 'hhhh'
+    img_dilation = cv2.dilate(RA, kernel2, iterations=1)
+    dst = cv2.filter2D(RA,-1,kernel,(100,9))
+    #print 'hhhh2222'
+    
+    imsave('map.png',img_dilation)
     k+=1
     b += tmp
+    #print 'hhhh333'
+    
 d = json.loads(b.decode('utf-8'))
